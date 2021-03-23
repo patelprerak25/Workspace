be79266152b7c64f7ee722d13dc0d280a7561c93
Prerak <patelprerak.25@gmail.com>
2021-03-23 11:27:05 +0530

    Initial commit

---
+++ EmpolyeeBAL/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpolyeeBAL
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ TryDI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using EmployeeDAL;
+using EmpolyeeBAL;
+
+namespace TryDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee objEmp = new EmpolyeeBAL.Employee();
+            objEmp.FirstName = "Prerak";
+            objEmp.LastName = "Patel";
+            objEmp.BirthDate = DateTime.Now;
+            objEmp.Email = "patelprerak@gmail.com";
+
+            EmployeeADO objEmpADO = new EmployeeADO();
+            objEmpADO.Add(objEmp);
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
